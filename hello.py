4539dd98d0bd038a2b75e04bed7c52162ad32c17
Moby-Dick-dot <65077726+Moby-Dick-dot@users.noreply.github.com>
2022-03-19 10:46:18 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,6 +6,6 @@ file = st.file_uploader('Upload An Image')
 if file:  # if user uploaded file
     img = Image.open(file)
 
-st.title("Here is the image you've selected")
+st.title("Here is the image you've selected lalalalal")
 resized_image = img.resize((336, 336))
 st.image(resized_image)
